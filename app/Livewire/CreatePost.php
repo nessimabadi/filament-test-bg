c5e3d176ab6f07e4c329014058634e31af8bfcff
Nessim Abadi <nessimabadi@gmail.com>
2026-01-05 10:04:44 -0500

    Livewire component

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -2,11 +2,42 @@
 
 namespace App\Livewire;
 
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Concerns\InteractsWithSchemas;
+use Filament\Schemas\Contracts\HasSchemas;
+use Filament\Schemas\Schema;
 use Illuminate\View\View;
 use Livewire\Component;
 
-class CreatePost extends Component
+class CreatePost extends Component implements HasSchemas
 {
+    use InteractsWithSchemas;
+
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                DatePicker::make('date')
+                    ->required(),
+            ])
+            ->statePath('data');
+    }
+
+    public function create(): void
+    {
+        dd($this->form->getState());
+    }
+
     public function render(): View
     {
         return view('livewire.create-post');
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,3 +1,11 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <form wire:submit="create">
+        {{ $this->form }}
+
+        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-600 mt-4">
+            Submit
+        </button>
+    </form>
+
+    <x-filament-actions::modals />
 </div>
